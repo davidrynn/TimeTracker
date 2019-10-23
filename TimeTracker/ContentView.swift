a8e24a9bb5efd4de25415dbcab807e8c26a3e221
David Rynn <davidrynn@gmail.com>
2019-10-22 23:23:09 -0400

    first commit

---
+++ TimeTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimeTracker
+//
+//  Created by David Rynn on 10/22/19.
+//  Copyright © 2019 David Rynn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
